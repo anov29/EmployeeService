51538195bdc49d769ec792d5eb244062295da406
Andrew Novitskiy <Andrew.Novitskiy@gmail.com>
2021-03-27 22:53:17 -0400

    Added error handeling

--- src/main/java/com/employee/web/controller/EmployeeController.java
+++ src/main/java/com/employee/web/controller/EmployeeController.java
@@ -4,13 +4,18 @@ import java.util.HashMap;
 
 import com.employee.web.model.Employee;
 import com.employee.web.services.EmployeeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class EmployeeController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(EmployeeController.class);
 
     private EmployeeService employeeService;
 
@@ -20,13 +25,27 @@ public class EmployeeController {
     }
 
     @GetMapping("/employees")
-    public HashMap<Long, Employee> getEmployees() {
-        return employeeService.getActiveEmployees();
+    public ResponseEntity<HashMap<Long, Employee>> getEmployees() {
+        try {
+            return new ResponseEntity<>(employeeService.getActiveEmployees(), HttpStatus.OK);
+        } catch (Exception e) {
+            LOGGER.error("Error in getEmployees: ", e);
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @GetMapping("/employees/{id}")
-    public Employee getEmployee(@PathVariable("id") Long id) {
-        return employeeService.getActiveEmployees().get(id);
-    }
+    public ResponseEntity<Employee> getEmployee(@PathVariable("id") Long id) {
+        try {
+            if (!employeeService.getActiveEmployees().containsKey(id)) {
+                LOGGER.info("Server retrieved bad request id {}", id);
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
+            return new ResponseEntity<>(employeeService.getActiveEmployees().get(id), HttpStatus.OK);
 
+        } catch (Exception e) {
+            LOGGER.error("Error in getEmployee: ", e);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
