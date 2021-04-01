9075d63aab07e9aad5ca9b246e80ce6bce5cdb09
Andrew Novitskiy <Andrew.Novitskiy@gmail.com>
2021-04-01 00:42:16 -0400

    fixed authentication

--- src/main/java/com/employee/web/security/WebSecurityConfig.java
+++ src/main/java/com/employee/web/security/WebSecurityConfig.java
@@ -3,23 +3,41 @@ package com.employee.web.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth)
+            throws Exception {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        auth.inMemoryAuthentication()
+                .withUser("spring")
+                .password(encoder.encode("secret"))
+                .roles("USER");
+    }
+
+
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .httpBasic()
+                    .and()
                 .authorizeRequests()
-                    .antMatchers(HttpMethod.GET)
-                    .authenticated()
+                    .antMatchers(HttpMethod.GET).permitAll()
                     .and()
                 .authorizeRequests()
                     .antMatchers(HttpMethod.PUT).permitAll()
@@ -28,22 +46,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                     .antMatchers(HttpMethod.POST).permitAll()
                     .and()
                 .authorizeRequests()
-                    .antMatchers(HttpMethod.DELETE)
-                    .authenticated()
+                    .antMatchers(HttpMethod.DELETE).authenticated()
                 .anyRequest().authenticated();
         http.csrf().disable();
-    }
-
-    @Bean
-    @Override
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("user")
-                        .password("password")
-                        .roles("USER")
-                        .build();
-
-        return new InMemoryUserDetailsManager(user);
     }
 }
